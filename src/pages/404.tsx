1aac56eccd010de04e7c6d9d96811b50c75c25bc
rdx <reydux95@gmail.com>
2023-03-02 00:05:19 +0100

    feat: redirect to home on 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router';
+import routes from '../utils/routes';
+import { useEffect } from 'react';
+
+export default function Custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(routes.home());
+  }, [router]);
+}
